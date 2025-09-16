9111c05a67ed6f42825ed01b5649b08a3f461e2f
estellad <estelladong729@gmail.com>
2025-09-16 10:21:57 +0200

    to add utils doc, text

--- vignettes/utils.R
+++ vignettes/utils.R
@@ -1,15 +1,3 @@
-library(SpatialExperiment)
-#library(ggspavis)
-library(sf)
-library(DropletUtils)
-library(SingleCellExperiment)
-library(dplyr)
-library(ggplot2)
-library(magick)
-library(scuttle)
-library(SpatialFeatureExperiment)
-library(Voyager)
-
 flip_sf_Y <- function(sf, type = "POINT", img_height, scalef){
   st_geometry(sf) <- st_sfc( 
     lapply(st_geometry(sf), function(geom) {
@@ -38,9 +26,12 @@ readVisiumHDCellSeg <- function(td, res){
   
   ## Coord ---------------------------------------------------------------
   # Read the GeoJSON file of cell segmentation
-  geo_data <- st_read(file.path(td, "segmented_outputs", "cell_segmentations.geojson"))
-  scalef <- rjson::fromJSON(file = file.path(td, "segmented_outputs", "spatial", "scalefactors_json.json"))
-  image <- image_read(file.path(td, "segmented_outputs", "spatial", png_name))
+  geo_data <- st_read(file.path(td, "segmented_outputs", 
+                                "cell_segmentations.geojson"))
+  scalef <- rjson::fromJSON(file = file.path(td, "segmented_outputs", "spatial", 
+                                             "scalefactors_json.json"))
+  image <- image_read(file.path(td, "segmented_outputs", 
+                                "spatial", png_name))
   info <- image_info(image)
   
   # Get centroid
@@ -51,7 +42,8 @@ readVisiumHDCellSeg <- function(td, res){
   
   
   ## Countmat ------------------------------------------------------------
-  countmat_file <- file.path(td, "segmented_outputs", "filtered_feature_cell_matrix.h5")
+  countmat_file <- file.path(td, "segmented_outputs", 
+                             "filtered_feature_cell_matrix.h5")
   vhdcellsce <- DropletUtils::read10xCounts(countmat_file, col.names = TRUE)
   
   colnames(vhdcellsce) <- sub("cellid_0*([0-9]+)-.*", "\\1", vhdcellsce$Barcode)
@@ -102,81 +94,43 @@ readVisiumHDCellSeg <- function(td, res){
   # Flip Centroid Y
   centroids <- colGeometries(vhdsfe)$centroids
   colGeometries(vhdsfe)$updatecentroids <- 
-    flip_sf_Y(centroids, type = "POINT", img_height = info$height, scalef = imgData(vhdsfe)$scaleFactor) 
+    flip_sf_Y(centroids, type = "POINT", img_height = info$height, 
+              scalef = imgData(vhdsfe)$scaleFactor) 
   
   # Flip Cellseg Y
   cellSeg <- colGeometries(vhdsfe)$cellSeg
   colGeometries(vhdsfe)$updatecellSeg <- 
-    flip_sf_Y(cellSeg, type = "POLYGON", img_height = info$height, scalef = imgData(vhdsfe)$scaleFactor) 
+    flip_sf_Y(cellSeg, type = "POLYGON", img_height = info$height, 
+              scalef = imgData(vhdsfe)$scaleFactor) 
   
   # Flip Centroid Y in `spatialCoords()`
   spatialCoords(vhdsfe)[, "pxl_row_in_fullres"] <- 
-    info$height/imgData(vhdsfe)$scaleFactor - spatialCoords(vhdsfe)[, "pxl_row_in_fullres"]
+    info$height/imgData(vhdsfe)$scaleFactor - 
+    spatialCoords(vhdsfe)[, "pxl_row_in_fullres"]
   
   # Add high res coords to `colData()`
-  vhdsfe[[paste0("pxl_col_in_", res)]] <- spatialCoords(vhdsfe)[, "pxl_col_in_fullres"] * imgData(vhdsfe)$scaleFactor
-  vhdsfe[[paste0("pxl_row_in_", res)]] <- spatialCoords(vhdsfe)[, "pxl_row_in_fullres"] * imgData(vhdsfe)$scaleFactor
+  vhdsfe[[paste0("pxl_col_in_", res)]] <- 
+    spatialCoords(vhdsfe)[, "pxl_col_in_fullres"] * imgData(vhdsfe)$scaleFactor
+  vhdsfe[[paste0("pxl_row_in_", res)]] <- 
+    spatialCoords(vhdsfe)[, "pxl_row_in_fullres"] * imgData(vhdsfe)$scaleFactor
   
   return(vhdsfe)
 }
 
-# vhdsfe <- readVisiumHDCellSeg(td)
-
-# ## Visualization with Voyager ------------------------------------------
-# vhdsfe <- logNormCounts(vhdsfe)
-# 
-# plotSpatialFeature(vhdsfe, features = "PIGR", exprs_values = "logcounts",
-#                    colGeometryName = "updatecentroids", image_id = "hires", size = 0.1)
-# 
-# plotSpatialFeature(vhdsfe, features = "PIGR", exprs_values = "logcounts",
-#                    colGeometryName = "updatecellSeg")
 
 
 subsetVisiumHD <- function(vhdsfe, xmin, xmax, ymin, ymax){
   # Subset to roi --------------------------------------------------------
-  # From OSTA Visium HD bin-level workflow: xmin: 4214.113 ymin: 3062.505 xmax: 4222.916 ymax: 3135.374
+  # From OSTA Visium HD bin-level workflow: xmin: 4214.113 ymin: 3062.505 
+                                          # xmax: 4222.916 ymax: 3135.374
   
-  vhdsfe_subset <- vhdsfe[, vhdsfe$pxl_col_in_hires >= xmin & vhdsfe$pxl_col_in_hires <= xmax &
-                            vhdsfe$pxl_row_in_hires >= ymin & vhdsfe$pxl_row_in_hires <= ymax]
+  vhdsfe_subset <- vhdsfe[, vhdsfe$pxl_col_in_hires >= xmin & 
+                            vhdsfe$pxl_col_in_hires <= xmax &
+                            vhdsfe$pxl_row_in_hires >= ymin & 
+                            vhdsfe$pxl_row_in_hires <= ymax]
   
   return(vhdsfe_subset)
 }
-
-# vhdsfe_subset <- subsetVisiumHD(vhdsfe, 
-#                                 xmin = 4217.001, xmax = 4462.094, 
-#                                 ymin = 2996.551, ymax = 3225.485)
-
-# dim(colGeometries(vhdsfe_subset)$updatecentroids)
-# dim(colGeometries(vhdsfe_subset)$updatecellSeg)
-# 
-# range(vhdsfe$pxl_col_in_hires)
-# range(vhdsfe$pxl_row_in_hires)
-# 
-# range(st_coordinates(colGeometries(vhdsfe)$updatecentroids)[, 1]) * imgData(vhdsfe)$scaleFactor
-# range(st_coordinates(colGeometries(vhdsfe)$updatecentroids)[, 2]) * imgData(vhdsfe)$scaleFactor
-# range(st_coordinates(colGeometries(vhdsfe)$centroids))
-# 
-# range(st_coordinates(colGeometries(vhdsfe)$updatecellSeg)[, 1]) * imgData(vhdsfe)$scaleFactor
-# range(st_coordinates(colGeometries(vhdsfe)$updatecellSeg)[, 2]) * imgData(vhdsfe)$scaleFactor
-# range(st_coordinates(colGeometries(vhdsfe)$cellSeg))
-
-
-# ## Visualization with Voyager ------------------------------------------
-# vhdsfe_subset <- logNormCounts(vhdsfe_subset)
-# 
-# plotSpatialFeature(vhdsfe_subset, features = "PIGR", exprs_values = "logcounts",
-#                    colGeometryName = "updatecentroids", image_id = "hires", size = 1) 
-# 
-# plotSpatialFeature(vhdsfe_subset, features = "PIGR", exprs_values = "logcounts",
-#                    colGeometryName = "updatecellSeg", image_id = "hires") + 
-#   scale_fill_gradientn(colors = pals::jet())
-#   scale_fill_viridis_c(option = "D")
-# 
-# plotSpatialFeature(vhdsfe, features = "PIGR", exprs_values = "logcounts",
-#                    colGeometryName = "updatecellSeg", image_id = "hires")
-
-
-
 
 
 
