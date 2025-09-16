8e48b8fb47c029f9ebaf81ed594a15778647e263
EllisPatrick <ellis.patrick@sydney.edu.au>
2025-09-16 19:42:42 +1000

    Compress data

---
+++ R/rctd_result_level1.R
@@ -0,0 +1,13 @@
+#' Example RCTD results (level 1)
+#'
+#' Results from running RCTD at level-1 granularity for the workshop.
+#'
+#' @format A Spatial Experiment
+#' @usage data(rctd_result_level1)
+#' @docType data
+#' @keywords datasets
+#' @source Workshop materials.
+#' @examples
+#' data(rctd_result_level1)
+#' str(rctd_result_level1)
+"rctd_result_level1"
---
+++ R/rctd_result_level2.R
@@ -0,0 +1,13 @@
+#' Example RCTD results (level 2)
+#'
+#' Results from running RCTD at level-2 granularity for the workshop.
+#'
+#' @format A Spatial Experiment
+#' @usage data(rctd_result_level2)
+#' @docType data
+#' @keywords datasets
+#' @source Workshop materials.
+#' @examples
+#' data(rctd_result_level2)
+#' str(rctd_result_level2)
+"rctd_result_level2"
